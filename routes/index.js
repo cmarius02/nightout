ba9ac1066f091948c7bd0e920244b7364bd7185c
Marius Cotofana <cmarius02@gmail.com>
2014-02-28 00:48:28 +0200

    Moved all to repo root because heroku.

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var Users = mongoose.model('Food');
+var fs = require('fs');
+var app = express();
+
+exports.index = function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var contents = fs.readFileSync("./public/template/samples/sample-04/index.html", "UTF-8");
+  res.end(contents);
+};
+
+exports.api = function(req, res) {
+  res.json({success: true});
+};
